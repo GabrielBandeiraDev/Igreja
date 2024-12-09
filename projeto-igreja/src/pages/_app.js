3c0493a7da19f4ac83f9cfb545f4aeb75690fa7a
mattcoelho09 <matheus.coelho@outlook.com>
2024-12-09 01:14:31 -0400

    oioi

---
+++ projeto-igreja/src/pages/_app.js
@@ -0,0 +1,22 @@
+// pages/_app.js
+import '../styles/escolha.css';
+import '../styles/globals.css'; // Importando o CSS Global
+import '../styles/inscricao.css';
+
+
+
+
+import { useEffect } from 'react';
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Carregar outras configurações globais, como fontes ou scripts se necessário
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
+
+
+
